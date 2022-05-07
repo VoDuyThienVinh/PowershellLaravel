628a1deeb7598ced8ac29b0dd47ae1854645fd04
V <thienvinhvpv@gmail.com>
2022-05-07 14:48:10 +0700

    Fix docker file

--- automationLaravel.ps1
+++ automationLaravel.ps1
@@ -686,8 +686,3 @@ workflows:
 
 Add-Content -Path ./.circleci/config.yml -Value $circleConfig
 
-#Docker-Compose
-docker-compose -f docker-compose.yml --env-file .env up -d
-docker-compose -f docker-compose.yml --env-file .env exec app composer install
-docker-compose -f docker-compose.yml --env-file .env exec app php artisan key:generate
-docker-compose -f docker-compose.yml --env-file .env exec app php artisan migrate
